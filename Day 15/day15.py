f1f472face1856379532a8e094e9a98cafda9996
Swithan22 <nathandebongnie@gmail.com>
2023-12-27 23:50:16 +0100

    Day 15 part 2
    Went on wrong path, then used dict for boxes

--- Day 15/day15.py
+++ Day 15/day15.py
@@ -10,10 +10,55 @@ def algorithm(clear_string, current_value):
     return current_value
 
 
-if __name__ == "__main__":
+def part1(input):
     results = []
     for string in input:
         current_value = 0
         results.append(algorithm(string, current_value))
-    print('part 1 : ' + str(sum(results)))
+    return results
+
+
+def lenses(input):
+    box = 0
+    boxes = {i: [] for i in range(256)}
+    for string in input:
+        current_value = 0
+        label = string.split("=")
+        if len(label) == 2:
+            box = algorithm(label[0], current_value)
+            exists = False
+            for i, b in enumerate(boxes[box]):
+                if label[0] == b[0]:
+                    boxes[box][i] = (label[0], label[1])
+                    exists = True
+                    break
+            if not exists:
+                boxes[box].append((label[0], label[1]))
+        else:
+            label = string.split('-')[0]
+            for bx in boxes.keys():
+                if len(boxes[bx]) > 0:
+                    for b in boxes[bx]:
+                        if label == b[0]:
+                            boxes[bx].remove(b)
+                            break
+    return boxes
+
+
+def part2(input):
+    total = 0
+    boxes = lenses(input)
+    for box in boxes:
+        if len(boxes[box]) > 0:
+            for i, b in enumerate(boxes[box]):
+                total += (box + 1) * (i+1) * int(b[1])
+    return total
+
+
+if __name__ == "__main__":
+    results1 = part1(input)
+    print('part 1 : ' + str(sum(results1)))
+
+    results2 = part2(input)
+    print('part 2 : ' + str(results2))
 
